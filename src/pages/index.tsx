745769c76bfa383cf51d6c84e07ec2da4cd1bfde
sathvikbhavaraju <sathvikb21@gmail.com>
2023-04-19 11:49:58 -0400

    Finished Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,36 +16,38 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className="flex flex-row items-center px-3">
-        <Link href="/">
+      <nav className="flex flex-row items-center justify-between px-3">
+        <Link href="/" className="flex flex-row items-center">
           <Image src={logo} alt="Testify" width={75} height={75} />
+          <h1 className="text-2xl font-bold text-[#EB584F] hover:text-[#C04841]">
+            Testify
+          </h1>
         </Link>
 
-        <ul className="flex grow flex-row justify-center space-x-10">
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-
-        <div>
+        <div className="">
           {sessionData ? (
             <button
               onClick={() => void signOut()}
-              className=""
+              className="rounded-lg bg-[#EB584F] px-6 py-2 text-white hover:bg-[#C04841]"
             >
               Sign out
             </button>
           ) : (
-              <div>
-                <Link href="/signup"></Link>
-                <button onClick={() => void signIn()} className="">
-                  Login
-                </button>
-              </div>
-            )}
+            <div className="space-x-5">
+              <Link
+                href="/signup"
+                className="text-md font-medium text-[#EB584F] hover:text-[#C04841]"
+              >
+                Sign up
+              </Link>
+              <button
+                onClick={() => void signIn()}
+                className="rounded-lg bg-[#EB584F] px-6 py-2 text-white hover:bg-[#C04841]"
+              >
+                Login
+              </button>
+            </div>
+          )}
         </div>
       </nav>
     </>
